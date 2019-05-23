a5a5915f6f5d99890f1ed6f4f780af2e36708730
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2019-05-22 23:25:19 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 _*_
+
+import numpy as np
+
+
+vinte_cinco_primeiros = np.random.randint(1, 80+1, size=(5, 5))
+jogos = np.array([vinte_cinco_primeiros])
+todos_numeros = np.arange(1, 81)
